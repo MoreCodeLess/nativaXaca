eace769cc46e9cd2e385b902cee732863c726749
asaldarriaga <saldarias753@outlook.com>
2023-11-10 14:56:16 -0500

    add app

---
+++ app/src/main/java/com/example/nativa/DialogCloseListener.java
@@ -0,0 +1,5 @@
+package com.example.nativa;
+import android.content.DialogInterface;
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ app/src/main/java/com/example/nativa/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.nativa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.RelativeLayout;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+      //  this.getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
